9f383a58f2cf5468ec019189c2e09a9d393829ca
Kishan919225-ai <kishanshrestha936@gmail.com>
2025-05-15 09:11:55 +0545

    Eh

---
+++ exception handling/main.py
@@ -0,0 +1,21 @@
+# Eh is to deal with exception in running
+try:
+    print(10/0)
+except Exception as e:
+    print(e)
+finally:
+    print("Done")
+print("we are learning exception handeling")
+
+
+# We don't always use EH it depends on system
+
+def add(x,y):
+    if y==0:
+        raise Exception ("y shouldn't be 0")
+    return x+y
+
+try:
+    print(add(10,0))
+except Exception as e:
+    print(e)
