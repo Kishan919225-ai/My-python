c2a108e5556b2834dcf0599c6619a3a3f2400612
Kishan919225-ai <kishanshrestha936@gmail.com>
2025-05-17 07:38:59 +0545

    programme

--- programme/ev-od.py
+++ programme/ev-od.py
@@ -1,22 +1,22 @@
-# # A progeamme that checks if the numbers are even of odd
+# # # A progeamme that checks if the numbers are even of odd
 
-print("------------------------EVEN ODD CHECKER----------------------------")
+# print("------------------------EVEN ODD CHECKER----------------------------")
 
-num_items=int(input("Ennter how many numbers you want to check: "))
-odd=[]
-even=[]
+# num_items=int(input("Ennter how many numbers you want to check: "))
+# odd=[]
+# even=[]
 
-for i in range(num_items):
-    num=int(input("Enter number: "))
+# for i in range(num_items):
+#     num=int(input("Enter number: "))
 
-    if num%2==0:
-        print(f"{num} is even")
-        even.append(num)
-    else:
-        print(f"{num} is odd")
-        odd.append(num)
-print("-----------------Result------------------")
-print("Even=",even)
-print("odd=",odd)
-print("Total even numbers= ",len(even))
-print("Total oddd numbers=",len(odd))
+#     if num%2==0:
+#         print(f"{num} is even")
+#         even.append(num)
+#     else:
+#         print(f"{num} is odd")
+#         odd.append(num)
+# print("-----------------Result------------------")
+# print("Even=",even)
+# print("odd=",odd)
+# print("Total even numbers= ",len(even))
+# print("Total oddd numbers=",len(odd))
---
+++ programme/multiplication.py
@@ -0,0 +1,21 @@
+# A programme that generates multiplication table
+
+# def multiplication_table():
+
+#     num=int(input("Enter number: "))
+#     for i in range(1,11):
+#         print(f"{num}*{i}={num*i}")
+
+# multiplication_table()
+
+# A programme that generates multiplication table upo the num
+
+# def multi_table():
+
+#     num_item=int(input("Enter the number upto which you want to genrate multiplicatuon table:"))
+#     for i in range(1,num_item+1):
+#         print(f"\n Multiplication table of {i}")
+#         for j in range(1,11):
+#             print(f"{i}*{j}={i*j}")
+
+# multi_table()
